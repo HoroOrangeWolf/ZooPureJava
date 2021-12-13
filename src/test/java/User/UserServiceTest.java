e58eeb3246471fbe59ed1ff974a24e3a22aec9a8
LeeTomahawk <bartek20122013@gmail.com>
2021-12-13 22:38:06 +0100

    User test

---
+++ src/test/java/User/UserServiceTest.java
@@ -0,0 +1,67 @@
+package User;
+
+import com.Michalski.Minner.Mozdzierz.Ozga.Animal.Section;
+import com.Michalski.Minner.Mozdzierz.Ozga.User.User;
+import com.Michalski.Minner.Mozdzierz.Ozga.User.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Optional;
+
+public class UserServiceTest {
+    private final UserService service = new UserService();
+
+    @Test
+    public void addUser(){
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 0);
+        Date date = cal.getTime();
+        User u1 = new User(1L,"siema",false, date,"user@test.com");
+        service.addUser(u1);
+
+        Optional<User> user = service.getUserByEmail(u1.getEmail());
+
+        Assertions.assertTrue(user.isPresent());
+        Assertions.assertEquals(u1,user.get());
+    }
+    @Test
+    public void removeUser(){
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 0);
+        Date date = cal.getTime();
+        User u1 = new User(1L,"siema",false, date,"user@test.com");
+        service.addUser(u1);
+
+        Optional<User> user = service.getUserByEmail(u1.getEmail());
+
+        Assertions.assertTrue(user.isPresent());
+
+        Assertions.assertEquals(u1,user.get());
+
+        service.removeUser(u1.getId());
+
+        Assertions.assertTrue(service.getUserByEmail(u1.getEmail()).isEmpty());
+    }
+    @Test void updateUser(){
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 0);
+        Date date = cal.getTime();
+        User u1 = new User(1L,"siema",false, date,"user@test.com");
+        service.addUser(u1);
+        User u2 = new User(u1.getId(),"siemaaaaa",false, date,"user@test.com");
+        service.updateUser(u2);
+        Assertions.assertEquals(u2,u1);
+    }
+    @Test void isUserBok(){
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 0);
+        Date date = cal.getTime();
+        User u1 = new User(1L,"siema",true, date,"user@test.com");
+        service.addUser(u1);
+
+        Assertions.assertTrue(service.isUserBok(u1.getId()));
+    }
+}
